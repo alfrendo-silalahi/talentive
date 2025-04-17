043041f09b297666cdd94f01c01375e2c3ecf880
Alfrendo Silalahi <alfrendos72@gmail.com>
2025-04-17 14:17:50 +0700

    initial project

---
+++ src/main/java/dev/alfrendosilalahi/talentive/TalentiveApplication.java
@@ -0,0 +1,13 @@
+package dev.alfrendosilalahi.talentive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalentiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalentiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/alfrendosilalahi/talentive/TalentiveApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.alfrendosilalahi.talentive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalentiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
